60ee14e4e0fbccaf2bc51037eae06f78cae480c8
raskour <rasmeetkour735@gmail.com>
2023-09-19 13:11:49 +1000

    fixed console warnings

--- src/commons/icons.jsx
+++ src/commons/icons.jsx
@@ -5,16 +5,16 @@ export function DeleteIcon(props) {
     <svg
       {...props}
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-trash"
+      className="icon icon-tabler icon-tabler-trash"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="#2c3e50"
       fill="none"
-      stroke-linecap="round"
+      strokeLinecap="round"
       aria-hidden="true"
-      stroke-linejoin="round"
+      strokeLinejoin="round"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M4 7l16 0" />
@@ -30,15 +30,15 @@ export function CartIcon(props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-shopping-cart"
+      className="icon icon-tabler icon-tabler-shopping-cart"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="#2c3e50"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       aria-hidden="true"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -54,15 +54,15 @@ export function HeartIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      class="icon icon-tabler icon-tabler-heart"
+      className="icon icon-tabler icon-tabler-heart"
       width="24"
       height="24"
       viewBox="0 0 24 24"
-      stroke-width="1.5"
+      strokeWidth="1.5"
       stroke="#2c3e50"
       fill="none"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       aria-hidden="true"
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
